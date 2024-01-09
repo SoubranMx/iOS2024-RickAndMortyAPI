a2a29bfd43990b8e964d9292cd891f2c5a001142
Uriel Soubran <uriel_bee15@hotmail.com>
2024-01-09 15:09:31 -0600

    RickAndMortyApp
    Package> https://github.com/lorenzofiamingo/swiftui-cached-async-image

---
+++ RickAndMorty/RickAndMorty/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Uriel Soubran on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CharactersListView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Uriel Soubran on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
